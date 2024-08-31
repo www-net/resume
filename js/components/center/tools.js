bcc631680c2b6e9c55a2eb84e97c347b59ec538f
www-net <78052177+www-net@users.noreply.github.com>
2024-08-31 14:01:43 +0300

    feat: add tools markup

--- js/components/center/tools.js
+++ js/components/center/tools.js
@@ -1,4 +1,22 @@
+import { toolDesignIcons, toolNoCodeIcons, toolOtherIcons } from "../../mock/constants";
+
+const createToolsMarkup = (src) => {
+  return (
+    `<li class="tools__icon">
+      <img src=${src} alt="иконка рабочего инструмента">
+    </li>`
+  )
+}
+
 export const createToolsTemplate = () => {
+
+
+
+  const toolDesignMarkup = toolDesignIcons.map((icon) => createToolsMarkup(icon)).join('\n'); 
+  const toolNoCodeMarkup = toolNoCodeIcons.map((icon) => createToolsMarkup(icon)).join('\n'); 
+  const toolOtherMarkup = toolOtherIcons.map((icon) => createToolsMarkup(icon)).join('\n'); 
+
+
   return (
     `<section class="tools">
         <h2 class="tools__title">Tools</h2>
@@ -6,46 +24,23 @@ export const createToolsTemplate = () => {
           <li class="tools__list-item">
             <h3 class="tools__item-title">design</h3>
             <ul class="tools__inner-item-list">
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
+              ${toolDesignMarkup}
+              </ul>
               </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
+              <li class="tools__list-item">
+              <h3 class="tools__item-title">no-code</h3>
+              <ul class="tools__inner-item-list">
+              
+              ${toolNoCodeMarkup}
+              
+              </ul>
               </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-            </ul>
-          </li>
-          <li class="tools__list-item">
-            <h3 class="tools__item-title">no-code</h3>
-            <ul class="tools__inner-item-list">
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-            </ul>
-          </li>
-          <li class="tools__list-item">
-            <h3 class="tools__item-title">artoficial intelligence</h3>
-            <ul class="tools__inner-item-list">
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
-              <li class="tools__icon"><img src="./icons/icon_adobe-photoshop.svg" alt="иконка рабочего инструмента">
-              </li>
+              <li class="tools__list-item">
+              <h3 class="tools__item-title">artoficial intelligence</h3>
+              <ul class="tools__inner-item-list">
+              
+              ${toolOtherMarkup}
+
             </ul>
           </li>
         </ul>
---
+++ js/mock/constants.js
@@ -0,0 +1,21 @@
+export const toolDesignIcons = [
+  "./icons/logo/logo-figma.svg",
+  "./icons/logo/logo-adobe-photoshop.svg",
+  "./icons/logo/logo-adobe-illustrator.svg",
+  "./icons/logo/logo-adobe-premiere.svg",
+  "./icons/logo/logo-notion.svg",
+  "./icons/logo/logo-meet.svg",
+]
+
+export const toolNoCodeIcons = [
+  "./icons/logo/logo-zapìer.svg",
+  "./icons/logo/logo-webflow.svg",
+  "./icons/logo/logo-framer.svg",
+  "./icons/logo/logo-wordpress.svg",
+]
+
+export const toolOtherIcons = [
+  "./icons/logo/logo-chatGPT.svg",
+  "./icons/logo/logo-copilot.svg",
+  "./icons/logo/logo-midjourney.svg",
+]
