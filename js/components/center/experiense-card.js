05989fa7298e696083591e31b276c7c17593c4c5
www-net <78052177+www-net@users.noreply.github.com>
2024-09-05 13:02:41 +0300

    refactor: improved the experience card code

--- js/components/center/experiense-card.js
+++ js/components/center/experiense-card.js
@@ -6,29 +6,30 @@ const desctiptionMarkup = (tasks) => {
       <li>${el}</li>
       `)
   }).join(`\n`)
-}
+};
 
 export const createExperienceCardTemplate = (dataList) => {
-  return dataList.reduce((acc, current) => {
+  return dataList.reduce((acc, current, idx) => {
     const { date, profession, organisation, clockLoad, tasks } = current;
 
-    console.log('cerr    ', current)
-
-    const dateStart = `${MONTHS[date.start.getMonth()].slice(0,3)}.${date.start.getFullYear()}`
+    const mostRecent = idx === 0 ? `experience__item--most-recent` : ``;
+    const dateStart = `${MONTHS[date.start.getMonth()].slice(0,3)}.${date.start.getFullYear()}`;
     const dateEnd = date.end ? `${MONTHS[date.end.getMonth()].slice(0,3)}.${date.end.getFullYear()}` : `Present`;
+    const workForm =  organisation ? `<p class="experience__further">${organisation}</p>`: ``;
     const description = desctiptionMarkup(tasks);
 
 
     return acc + (
-      `<li class="experience__item">
+      `<li class="experience__item ${mostRecent}">
             <div class="experience__item-head">
-              <p class="experience__item_date">${dateStart} - ${dateEnd}</p>
+              <p class="experience__time-wrapper"><time class="experience__item_date">${dateStart}</time> - <time class="experience__item_date">${dateEnd}</time></p>
+              ${idx === 0 ? `<b class="most-recent">most recent</b>` : ``}
             </div>
             <div class="experience__item-main">
               <div>
                 <h3 class="experience__item-name">${profession}</h3>
                 <div class="experience__item-box">
-                  ${organisation ? `<p class="experience__further">${organisation}</p>`: ``}
+                  ${workForm}
                   <p class="experience__further">${clockLoad}</p>
                 </div>
               </div>
@@ -41,4 +42,4 @@ export const createExperienceCardTemplate = (dataList) => {
           </li>\n`
     )
   },``);
-}
+};
